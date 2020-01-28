8f44dab0ee38c67a8bd7d173a9c8366a454b5b82
Rafael <rafael.cruz@somosiris.com>
2020-01-28 17:55:29 -0300

    Auth, mutations, updating store

--- howtographql/src/components/App.js
+++ howtographql/src/components/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
 import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
+import Login from "./Login";
+import Search from "./Search";
+
+import Header from "./Header";
 
 class App extends Component {
   render() {
-    return <LinkList />;
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/search" component={Search} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
--- howtographql/src/index.js
+++ howtographql/src/index.js
@@ -3,29 +3,44 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 
 // 1
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { AUTH_TOKEN } from "./constants";
 
 // 2
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
 // 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 serviceWorker.unregister();
