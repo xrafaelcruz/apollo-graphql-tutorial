fe5783a42494ccd42aad30f1c046c12c396c03a0
Rafael <rafael.cruz@somosiris.com>
2020-01-29 19:50:10 -0300

    search, subscriptions, pagination

--- server/src/resolvers/Subscription.js
+++ server/src/resolvers/Subscription.js
@@ -1,26 +1,31 @@
 function newLinkSubscribe(parent, args, context, info) {
-  return context.prisma.$subscribe.link({ mutation_in: ['CREATED'] }).node()
+  const val = context.prisma.$subscribe
+    .link({ mutation_in: ["CREATED"] })
+    .node();
+  console.log(val);
+  return val;
 }
 
 const newLink = {
   subscribe: newLinkSubscribe,
   resolve: payload => {
-    return payload
-  },
-}
+    console.log(payload);
+    return payload;
+  }
+};
 
 function newVoteSubscribe(parent, args, context, info) {
-  return context.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node()
+  return context.prisma.$subscribe.vote({ mutation_in: ["CREATED"] }).node();
 }
 
 const newVote = {
   subscribe: newVoteSubscribe,
   resolve: payload => {
-    return payload
-  },
-}
+    return payload;
+  }
+};
 
 module.exports = {
   newLink,
-  newVote,
-}
+  newVote
+};
