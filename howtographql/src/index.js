a4b193987f1476046fbabaef3a0bb606e25e820d
Rafael <rafael.cruz@somosiris.com>
2020-01-29 19:49:46 -0300

    search, subscriptions, pagination

--- howtographql/src/index.js
+++ howtographql/src/index.js
@@ -5,6 +5,10 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+
 // 1
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
@@ -28,9 +32,27 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-// 3
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
 
